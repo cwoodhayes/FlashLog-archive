cwoodhayes/FlashLog-archive

commit 00aeca5113161c2a88076b7c4f1428f5eccecf97

Use compile-time checks to see if the BlockDevice_t is a SPIFBlockDevice (#13)
* Use compile-time checks to see if the BlockDevice_t is a SPIFBlockDevice

* remove unneeded scoping & indentation

testsuite/FlashLogTestSuite.cpp
@@ -578,17 +578,24 @@ int FlashLogHarness::test_writeAtLargeAddress(){
     return 0;
 }
 
-int FlashLogHarness::test_bulkErase(){
-    if (FlashLogConfig::isSPIFlash)
-    {
-        SPIFBlockDevice *spiFlashDev = reinterpret_cast<SPIFBlockDevice*>(harnessBlockDev.get());
-        spiFlashDev->bulk_erase();
-        test_check_erase();
-    }
-    else
-    {
-        printf("Bulk erase not implemented!\r\n");
-    }
+int FlashLogHarness::test_bulkErase()
+{
+    /* If SPIFBlockDevice is not defined anywhere, pre-declare it */
+    class SPIFBlockDevice;
+
+    /* Check if the type of the block device is a SPIFBlockDevice */
+    static constexpr bool IS_SPIFBLOCKDEVICE
+        = std::is_same<FlashLogConfig::BlockDevice_t, SPIFBlockDevice>();
+    (void)IS_SPIFBLOCKDEVICE; // ignore non-used warning
+
+/* Use compile-time checks to see if we can use SPIFBlockDevice::bulk_erase */
+#if IS_SPIFBLOCKDEVICE
+    SPIFBlockDevice *spiFlashDev = reinterpret_cast<SPIFBlockDevice*>(harnessBlockDev.get());
+    spiFlashDev->bulk_erase();
+    test_check_erase();
+#else
+    printf("Bulk erase not implemented!\r\n");
+#endif
 
     return 0;
 }

commit a2ce61ff0bc2fd2378b4e7ff6cab8dd3de308d03

Use C++ 17, constexpr-if, and templates to check if we can use SPIFBlockDevice::bulk_erase (#15)
* Use C++ 17, constexpr-if, and templates to check if we can use SPIFBlockDevice::bulk_erase

This fixes a bug introduced by #13 where a constexpr variable was used in a #if statement. This could not possibly evaluate to true (since it is a variable even though it is constexpr)

* Don't pre-declare the class inside the function

* Use correct words for forward-declaring

* consistent formatting

* 1 more tab

* Avoid warnings by using the template parameter instead of specifically SPIFBlockDevice

CMakeLists.txt
@@ -34,3 +34,5 @@ target_include_directories(flashlog PUBLIC ${CONFIG_HEADER_DIR})
 add_mbed_executable(test_flash ${TEST_FLASH_SOURCES})
 target_link_libraries(test_flash flashlog ${FLASHLOG_TEST_HARNESS_LIBRARIES})
 target_compile_options(test_flash PRIVATE -Wno-unknown-pragmas)
+
+target_compile_features(test_flash PRIVATE cxx_std_17)

testsuite/FlashLogTestSuite.cpp
@@ -578,24 +578,26 @@ int FlashLogHarness::test_writeAtLargeAddress(){
     return 0;
 }
 
+/* If SPIFBlockDevice is not defined anywhere, forward-declare it */
+class SPIFBlockDevice;
+
+template <typename T>
 int FlashLogHarness::test_bulkErase()
 {
-    /* If SPIFBlockDevice is not defined anywhere, pre-declare it */
-    class SPIFBlockDevice;
-
     /* Check if the type of the block device is a SPIFBlockDevice */
-    static constexpr bool IS_SPIFBLOCKDEVICE
-        = std::is_same<FlashLogConfig::BlockDevice_t, SPIFBlockDevice>();
-    (void)IS_SPIFBLOCKDEVICE; // ignore non-used warning
+    constexpr bool IS_SPIFBLOCKDEVICE = std::is_same<SPIFBlockDevice, T>();
 
-/* Use compile-time checks to see if we can use SPIFBlockDevice::bulk_erase */
-#if IS_SPIFBLOCKDEVICE
-    SPIFBlockDevice *spiFlashDev = reinterpret_cast<SPIFBlockDevice*>(harnessBlockDev.get());
-    spiFlashDev->bulk_erase();
-    test_check_erase();
-#else
-    printf("Bulk erase not implemented!\r\n");
-#endif
+    /* Use compile-time checks to see if we can use SPIFBlockDevice::bulk_erase */
+    if constexpr (IS_SPIFBLOCKDEVICE)
+    {
+        T* spiFlashDev = reinterpret_cast<T*>(harnessBlockDev.get());
+        spiFlashDev->bulk_erase();
+        test_check_erase();
+    }
+    else
+    {
+        printf("Bulk erase not implemented!\r\n");
+    }
 
     return 0;
 }

testsuite/FlashLogTestSuite.h
@@ -132,10 +132,17 @@ class FlashLogHarness : public FlashLog
 		/**
 		 * @brief      Confirm that the flashlog's checksum calculator is working
 		 *
+		 * @tparam T We use a template parameter here as a way to enforce that only one branch of
+		 *           the constexpr-if statement is parsed by the compiler. This template argument is
+		 *           never used, but T is used in the function to make the compiler respect our
+		 *           constexpr-if. For more information on why this is necessary, see
+		 *           https://stackoverflow.com/questions/50051473/if-constexpr-in-c17-does-not-work-in-a-non-templated-function
+		 *
 		 * @return     err
 		 */
+		template <typename T = FlashLogConfig::BlockDevice_t>
 		int test_bulkErase();
-		
+
 		/**
 		 * @brief      Confirm that 32 bit addressing works
 		 *